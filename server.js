71d07e4df9a7c48ced0d69555605c582d5b225ae
OnigiriJack <jackmihalik@icloud.com>
2020-11-02 18:33:44 +0900

    able to get today and yesterday in YYYY-MM-DAY format for axios query

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
 
+let today =  new Date();
+let yesterday =  new Date(today);
+yesterday.setDate(yesterday.getDate() - 1)
+
+const ISOTODAY = today.toISOString().split('T')[0];
+const ISOYESTERDAY = yesterday.toISOString().split('T')[0];
+
+const NASA_URL =  `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=${ISOTODAY}&api_key=${process.env.NASA_API_KEY}`; 
 app.use(express.static(__dirname + "/dist"));
 
 app.use("/api", (req, res) => {
-    res.send("OIOIOI ☄️");
+  
+    res.send("IOI ☄️"+ ISOTODAY + "  "+ISOYESTERDAY);
 })
+
+
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
