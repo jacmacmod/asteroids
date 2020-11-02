344af7d6936ca3db2e0c394f34fffd09e6011306
OnigiriJack <jackmihalik@icloud.com>
2020-11-02 19:24:00 +0900

    axios call to retreive data from nasa

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require("dotenv").config();
 
-let today =  new Date();
-let yesterday =  new Date(today);
-yesterday.setDate(yesterday.getDate() - 1)
+function createNASAURL() {
+    let today =  new Date();
+    let yesterday =  new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1)
+    const ISOTODAY = today.toISOString().split('T')[0];
+    const ISOYESTERDAY = yesterday.toISOString().split('T')[0]; 
+    return `https://api.nasa.gov/neo/rest/v1/feed?start_date=${ISOYESTERDAY}&end_date=${ISOTODAY}&api_key=${process.env.NASA_API_KEY}`;
+}
+  
+let NASA_URL =  createNASAURL();
 
-const ISOTODAY = today.toISOString().split('T')[0];
-const ISOYESTERDAY = yesterday.toISOString().split('T')[0];
+async function getAsteroids(URL) {
+    const { data: asteroids } = await axios.get(URL);
+ return asteroids;
+}
 
-const NASA_URL =  `https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=${ISOTODAY}&api_key=${process.env.NASA_API_KEY}`; 
+
+
 app.use(express.static(__dirname + "/dist"));
 
 app.use("/api", (req, res) => {
-  
-    res.send("IOI ☄️"+ ISOTODAY + "  "+ISOYESTERDAY);
+let asteroids = getAsteroids(NASA_URL)
+    res.json(asteroids);
 })
 
 
