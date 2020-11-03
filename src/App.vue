21cff96abd0d3d4a3d01f5edf289c3eee0689f43
OnigiriJack <jackmihalik@icloud.com>
2020-11-03 22:17:28 +0900

    trying to implement css grid

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,11 @@
   <div id="app">
 
 
-    ☄️ ☄️ ☄️ ☄️ ☄️ ☄️ ☄️ ☄️
+<div class="header">
   <button v-on:click="compare">COMPARE</button>
-
-<select v-model="selectedAsteroid" >
+  <select v-model="selectedAsteroid" >
   <option disabled value="">Please select Asteroid☄️</option>
-  <option v-for="asteroid of asteroids" :key="asteroid" >
-  {{asteroid.name}}
+  <option v-for="asteroid of asteroids" :key="asteroid" >{{asteroid.name}}
   </option>
 </select>
 
@@ -18,11 +16,15 @@
 <div> 
   Closest Object: {{EarthObject}}
 </div>
+
+
+</div>
+
 <div class="comparison-area">
   <div class="asteroid" >{{selectedAsteroid}}</div>
   <div class="asteroid scaledObject" :style="cssProps">{{EarthObject.name}} </div>
 </div>
-<h2>{{scale}}</h2>
+<h2>{{EarthObject.name}} is {{scale}} times the size of the Asteroid {{selectedAsteroid}}</h2>
   </div>
 </template>
 
@@ -33,7 +35,7 @@ export default {
   name: 'App',
   data: () => ({
     asteroids: [1,2,3],
-    scale: 2,
+    scale: 1,
     selectedAsteroid: "",
     asteroid: "",
     EarthObject: "",
@@ -77,16 +79,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: #3b404e;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   color: white;
-  margin-top: 60px;
-}
-.comparions-area {
+  height: 100%;
+  width: 100%;
   display: grid;
-  grid-template-columns: 150px 150px 150px;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+}
+.header {
+  grid-row: 1 / 2;
+  grid-column: 1 / 4;
+}
+
+.comparions-area {
+  grid-row: 1 / 2;
+  grid-column: 1 / 4;
 }
 .asteroid {
   margin: 100px;
