be1ea88d1e0500a9e8c33bbf2ad086107fd79904
OnigiriJack <jackmihalik@icloud.com>
2020-11-04 11:28:41 +0900

    changed knexfile to include heroku url

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 module.exports = {
     client: 'pg',
     connection: 
-        process.env.DATABASE_URL ||
+        process.env.HEROKU_POSTGRESQL_CHARCOAL_URL ||
         `postgres://${process.env.USER}@127.0.0.1:5432/asteroids`,
         searchPath: "public",
     migrations: "./migrations",
